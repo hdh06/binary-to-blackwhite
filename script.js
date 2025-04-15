d68d271efe6f1fc8838c10672104550792facd12
_HDH <45970570+hdh06@users.noreply.github.com>
2025-04-15 14:10:25 -0700

    Added script.js, a summit button, some test js code and input for nxm

---
+++ script.js
@@ -0,0 +1,9 @@
+function convert(){
+    const x = document.getElementById("binary_code").value;
+    let div = document.createElement("div");
+    div.className= "black box";
+    document.body.appendChild(div);
+    // alert(document.getElementById("binary_code").value);
+    // alert(document.getElementById("height").value);
+    // alert(document.getElementById("height").value);
+}
