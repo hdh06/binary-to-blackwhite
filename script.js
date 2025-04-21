41df43486ad6599403237c2b6d9961b3bf577233
_HDH <45970570+hdh06@users.noreply.github.com>
2025-04-21 15:39:57 -0700

    Add flex image div

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 function convert(){
     const x = document.getElementById("binary_code").value;
     let div = document.createElement("div");
-    div.className= "black box";
-    document.body.appendChild(div);
+    div.classList.add("black", "box");
+    document.getElementById("image").appendChild(div);
     // alert(document.getElementById("binary_code").value);
     // alert(document.getElementById("height").value);
     // alert(document.getElementById("height").value);
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+    const sendBtn = document.getElementById("convertBtn");
+  
+    sendBtn.addEventListener("click", () => {
+        convert();
+    });
+});
